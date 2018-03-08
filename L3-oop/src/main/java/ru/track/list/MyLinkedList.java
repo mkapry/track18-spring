d6869e64396c7cb1f3d815bf1909a3b347cfc326
mkapry <mkapry@yandex.ru>
2018-03-08 21:04:16 +0300

    linkedlist

--- L3-oop/src/main/java/ru/track/list/MyLinkedList.java
+++ L3-oop/src/main/java/ru/track/list/MyLinkedList.java
@@ -7,6 +7,9 @@ import java.util.NoSuchElementException;
  * Односвязный список
  */
 public class MyLinkedList extends List {
+    private static ListElement head=null;
+    private static size=0;
+
 
     /**
      * private - используется для сокрытия этого класса от других.
@@ -28,20 +31,66 @@ public class MyLinkedList extends List {
 
     @Override
     void add(int item) {
+        if(head==0)
+        {
+            head=this;
+            next=0;
+            prev=0;
+            val=item;
+            size++;
+        } else{
+            next=head;
+            head=this;
+            next.prev=this;
+            prev=0;
+            this.val=item;
+            size++;
+        }
+
     }
 
     @Override
     int remove(int idx) throws NoSuchElementException {
-        return 0;
+        if(idx>size) throw NoSuchElementException;
+        NODE t = head;
+        int i=0;
+        if(head == null)
+            return 0;
+        for (i=0;i<idx+1;i++)
+                t=t.next;
+        while (t.next!==null)
+
+        {
+            t.next = t.next.next;
+            t.next.prev = t.prev;
+            size--;
+        }
+                return 0;
+            }
+
     }
 
     @Override
     int get(int idx) throws NoSuchElementException {
-        return 0;
+        NODE t = head;
+
+        if(idx>size) throw NoSuchElementException;
+        if(head == null)
+            return 0;
+        for (i=0;i<idx+1;i++)
+            t=t.next;
+        return t;
     }
 
     @Override
     int size() {
-        return 0;
+        int i=1;
+        NODE c=head;
+        while (c!=this)
+        {i++;
+        c=c.next;
+        }
+        return i;
     }
+
 }
