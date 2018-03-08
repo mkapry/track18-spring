acf3e961a2d24285a33fd420eaba28f011838557
mkapry <mkapry@yandex.ru>
2018-03-08 21:03:20 +0300

    list

--- L3-oop/src/main/java/ru/track/list/MyArrayList.java
+++ L3-oop/src/main/java/ru/track/list/MyArrayList.java
@@ -10,32 +10,55 @@ import java.util.NoSuchElementException;
  * - с аргументом - начальный размер массива
  */
 public class MyArrayList extends List {
+    private final int INIT_SIZE = 16;
+    private array;
+    private int pointer = 0;
+    private static int size;
 
     public MyArrayList() {
+        Object[] array = new Object[INIT_SIZE];
+        size=0;
 
     }
 
     public MyArrayList(int capacity) {
-
+        Object[] array = new Object[capacity];
+        size=0;
     }
 
     @Override
     void add(int item) {
+        if (pointer==INIT_SIZE) {
+            Object[] arraynew = new Object[INIT_SIZE+ 1];
+            System.arraycopy(array,0,arraynew,0)
+
+        }
+
+
+            array[pointer++] = item;
+        size++;
 
     }
 
     @Override
     int remove(int idx) throws NoSuchElementException {
+        if(idx>size) throw NoSuchElementException;
+        for (int i = idx; i<pointer; i++)
+            array[i] = array[i+1];
+        array[pointer] = null;
+        pointer--;
+        size--;
         return 0;
     }
 
     @Override
     int get(int idx) throws NoSuchElementException {
-        return 0;
+        if(idx>size) throw NoSuchElementException;
+        return array[idx];
     }
 
     @Override
     int size() {
-        return 0;
+        return pointer;
     }
 }
